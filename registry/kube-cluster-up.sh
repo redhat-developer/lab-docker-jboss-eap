9677cac40ebf354bdfaed5c043ac8e66ca774bab
Pete Muir <pmuir@bleepbleep.org.uk>
2015-05-01 14:07:04 +0100

    Inline all the kube setup

--- registry/kube-cluster-up.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-for SERVICES in etcd kube-apiserver kube-controller-manager kube-scheduler docker kube-proxy.service kubelet.service; do 
-    sudo systemctl restart $SERVICES
-    sudo systemctl enable $SERVICES
-    sudo systemctl status $SERVICES 
-done
-
