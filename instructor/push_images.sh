42b443c61230f1af50aec374a61993aa40450652
tqvarnst <tqvarnst@redhat.com>
2015-05-27 15:56:30 +0200

    Add more images to push during setup

--- instructor/push_images.sh
+++ instructor/push_images.sh
@@ -5,16 +5,19 @@ repo="localhost:5000"
 
 PULL_IMAGES='registry.access.redhat.com/rhel7.1:latest ce-registry.usersys.redhat.com/jboss-eap-6/eap:6.4 ce-registry.usersys.redhat.com/jboss-webserver-3/httpd:3.0'
 
-for IMAGE in ${PULL_IMAGES}; do 
+for IMAGE in ${PULL_IMAGES}; do
     if [[ $IMAGE =~ $regex ]]; then
         server=${BASH_REMATCH[1]}
         name=${BASH_REMATCH[2]}
         tag=${BASH_REMATCH[3]}
     fi
-    docker pull $IMAGE
+
+    #Only pull if the images doesn't exists locally
+    if [ $(docker images | grep $server/$name | grep $tag | wc -l) -lt 1 ]; then
+        docker pull $IMAGE
+    fi
     id_str=`docker inspect $IMAGE | grep Id`
     id=${id_str:11:-2}
     docker tag -f $id $repo/$name:$tag
     docker push $repo/$name:$tag
 done
-
