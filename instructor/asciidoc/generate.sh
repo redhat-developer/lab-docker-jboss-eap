026d22141ef22e399063b880da5ccc6ddbab6e54
Pete Muir <pmuir@bleepbleep.org.uk>
2015-06-09 11:36:02 +0100

    Fixes #17 and #18

--- instructor/asciidoc/generate.sh
+++ instructor/asciidoc/generate.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 asciidoctor /documents/lab.adoc -o /output/lab.html
 asciidoctor -b docbook /documents/lab.adoc -o /output/lab.xml
+cp -r /documents/images /output
 cd /output
 fopub lab.xml
 
